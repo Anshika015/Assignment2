bf02bf0741d7fc98aa5b9b2cb3a154fd680852b3
Anshika015 <agarg6_be24@thapar.edu>
2025-08-13 10:18:16 +0530

    ques3.cpp

--- ques3.cpp
+++ ques3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int findMissingIndexCheck(int arr[], int n) {
+int MissingIndexCheck(int arr[], int n) {
     int start = arr[0];
     for (int i = 0; i < n; i++) {
         if (arr[i] != start + i) {
@@ -15,8 +15,8 @@ int main() {
     int arr[] = {1, 2, 3, 4, 6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Method 2 (Index Check): Missing number is " 
-         << findMissingIndexCheck(arr, n) << endl;
+    cout <<MissingIndexCheck(arr, n) << endl;
 
     return 0;
 }
+
