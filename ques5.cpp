56abd9e46832a2ea1a94222fd9f588255339e4ac
Anshika015 <agarg6_be24@thapar.edu>
2025-09-10 11:42:59 +0530

    ques5.cpp

--- ques5.cpp
+++ ques5.cpp
@@ -24,12 +24,14 @@ int main() {
     cout << "Enter tri-diagonal elements: ";
     for (int i = 0; i < 3 * n - 2; i++) cin >> tri[i];
     cout << "Tri-diagonal Matrix:" << endl;
+    int index = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (i == j) cout << tri[n - 1 + i] << " ";
-            else if (i == j + 1) cout << tri[i - 1] << " ";
-            else if (j == i + 1) cout << tri[2 * n - 1 + i] << " ";
-            else cout << 0 << " ";
+            if (j >= i - 1 && j <= i + 1) { 
+                cout << tri[index++] << " ";
+            } else {
+                cout << 0 << " ";
+            }
         }
         cout << endl;
     }
